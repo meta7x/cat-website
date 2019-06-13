8e52a18df8fd7d869d708939736cd49ee1a53d73
Dimitri <dimitri@student-net-cx-1152.ethz.ch>
2019-06-13 16:41:58 +0200

    initial

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,86 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'CAT',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['link', { rel: 'icon', href: '/icons/favicon.ico' }],
+    // web app manifest for PWA support
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    // ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    logo: 'https://static.vis.ethz.ch/img/spirale_black.svg',
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Guide',
+        link: '/guide/',
+      },
+      {
+        text: 'Tools',
+        link: '/tools/',
+      },
+      {
+        text: 'Apps',
+        link: '/apps/'
+      },
+      {
+        text: 'GitLab',
+        link: 'https://gitlab.ethz.ch/vis/cat'
+      },
+      {
+        text: 'VIS',
+        link: 'https://vis.ethz.ch'
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+    ['@vuepress/pwa', {
+      serviceWorker: true,
+      updatePopup: true
+    }]
+  ]
+}
---
+++ docs/README.md
@@ -0,0 +1,13 @@
+---
+home: true
+heroImage: /cat-kontur.png
+tagline: Homepage of the Computer Application Team
+features:
+- title: Feature 1 Title
+  details: Feature 1 Description
+- title: Feature 2 Title
+  details: Feature 2 Description
+- title: Feature 3 Title
+  details: Feature 3 Description
+footer: Copyright © 2019 Verein Informatik Studierende
+---
