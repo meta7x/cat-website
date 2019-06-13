3104b253b1f103c48e73ec843c2be8eb5e44fa55
Dimitri <dimitri@student-net-cx-1152.ethz.ch>
2019-06-13 17:59:19 +0200

    config changes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -81,9 +81,6 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
-    ['@vuepress/pwa', {
-      serviceWorker: true,
-      updatePopup: true
-    }]
+    '@vuepress/pwa'
   ]
 }
